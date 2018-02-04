408ff8b66d1714d82d399cb373c6a749b7bfa71a
Chris Nelson <chris@gaslight.co>
2018-02-03 19:00:44 -0500

    initial test fail

--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -4,4 +4,9 @@ import static org.junit.Assert.*;
 
 public class TriangleTest {
 
+    @Test
+    public void testThatValidTriangleAreValid() {
+        Triangle triangle = new Triangle(3, 4, 5);
+        assertTrue(triangle.isValid());
+    }
 }
