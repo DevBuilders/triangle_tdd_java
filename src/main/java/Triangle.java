6cc5b0b45a6131ca0dc84742c818042c7a579c2e
Chris Nelson <chris@gaslight.co>
2018-02-03 19:36:28 -0500

    Make the first legit test pass

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,8 +1,16 @@
 public class Triangle {
+
+    private int side1;
+    private int side2;
+    private int side3;
+
     public Triangle(int side1, int side2, int side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
     }
 
     public boolean isValid() {
-        return true;
+        return this.side1 + this.side2 >= this.side3;
     }
 }
